fd05894648f0c4c06dabaae62acc12ad766d8fa2
NoobyToosday <j.n.westgate@gmail.com>
2022-06-09 07:10:58 -0400

    Fix multiple bugs

--- src/VFSPipe.jl
+++ src/VFSPipe.jl
@@ -9,9 +9,9 @@ stripWidthInM = [10.0]
 
 # The path to the working directory - should contain the .SWI or .PWC PWC file, and the output files from the original run (.zts, .inp, vvwmtransfer.txt).
 # NOTE: If an .SCN file using the filename specified in the PWC run is located in workingPath, it will supersede any in the folder specified by scenarioPath
-workingPath = "Z:\\SharedwithVM\\VFS\\EPA 30 Day\\" # must end in a double back-slash or slash
+workingPath = "Z:\\SharedwithVM\\VFS\\Phase Three\\Test Case\\" # must end in a double back-slash or slash
 # Name of the .SWI or .PWC file
-pwcName = "MScorn"
+pwcName = "TestCasePWC2.0-AB"
 
 # *******************************************************************************************************************************************************************
 #Less often changed are the paths to unchanging files
@@ -19,20 +19,18 @@ pwcName = "MScorn"
 exePath = "Z:\\SharedwithVM\\VFS\\executables\\PWC2VFSMOD45\\" # must end in a double back-slash or slash
 
 # The location of the pre-run .zts files where θ, the water content of the VFS soil, is found.
-thetaPath = "Z:\\SharedwithVM\\VFS\\EPA 30 Day\\" # must end in a double back-slash or slash
+thetaPath = "Z:\\SharedwithVM\\VFS\\CanadianThetaZts\\" # must end in a double back-slash or slash
 
 # The path to the standard scenarios is not required any longer - all scenario information is read from the .PWC or .SWI file
-# If a .SCN file by the name of that used for the PWC run is found in the workingPath, that is used, otherwise a version is required in scenarioPath
-# scenarioPath = "Z:\\SharedwithVM\\Scenarios\\" # must end in a double back-slash or slash
-
 
 # *******************************************************************************************************************************************************************
 # The following all have defaults, but are of interest for the model's sensitivity to them
-# If there's a proper hourly precipitation file (UPF), use that, otherwise generate the precipitation event programmatically from the daily value
+# If there's a proper hourly precipitation file (HPF), use that, otherwise generate the precipitation event programmatically from the daily value
 useHPF = false
 stormLengthInHours = 8 #only read when useHPF is false - RECOMMENDED CHOICE IS 8
 
 pesticideEquation = 3 # 1:Sabbagh;2:refitSabbagh;3:mass-bal.;4:Chen - RECOMMENDED CHOICE IS 3
+remobilizationFlag = 1 # Recommended choice is 1 - only the part of the dissolved pesticide in mixing layer in equilibrium with moisture content is remobilized at the start of the next event
 
 # Ksat - Any positive value in m/s
 # Set to any value -9999 or less to use the lookup table method (based on soil texture pulled from the PWC scenario)
@@ -72,7 +70,7 @@ for width in stripWidthInM
         end
 
         # All of the user input variables collected here are passed to the other module in a single struct called a userInputs
-        usInp = userInputs(stripWidthInM = width, workingPath = workingPath, pwcName = pwcName, useHPF = useHPF, stormLengthInHours = stormLengthInHours, exePath = exePath, thetaPath = thetaPath, pesticideEquation = pesticideEquation, Ksat = Ksat, shapeFlag = shapeFlag, slope = slope, isFirstRun = firstRun, isLastRun = lastrun)#, scenarioName = scenarioName, scenarioPath = scenarioPath)
+        usInp = userInputs(stripWidthInM = width, workingPath = workingPath, pwcName = pwcName, useHPF = useHPF, stormLengthInHours = stormLengthInHours, exePath = exePath, thetaPath = thetaPath, pesticideEquation = pesticideEquation, Ksat = Ksat, shapeFlag = shapeFlag, slope = slope, isFirstRun = firstRun, isLastRun = lastrun, remobilizationFlag = remobilizationFlag)#, scenarioName = scenarioName, scenarioPath = scenarioPath)
 
         #This runs VFSMOD for all runoff events, rewrites the zts file and returns the string to run VVWM
         newVVWM, oldTXT, newTXT = vfsMain(usInp)
